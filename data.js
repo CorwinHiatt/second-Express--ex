c56eecd685ea3acb230d483b6316d9669eb2933d
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-17 15:58:50 -0400

    second API

---
+++ data.js
@@ -0,0 +1,16 @@
+export default [{
+    "name": "brad jones",
+    "age" : 30,
+    "bday":"oct 20, 1992",
+},
+{
+    "name": 'corwin🤯',
+    "age" : 26,
+    "bday": 'jan 25',
+},
+{
+    "name": 'fanny',
+    "age" : 28,
+    "bday": 'jan 28🤯',
+}
+]
